f70d67d9af2767c0b5402587875e7a706e11a5b3
Deepak gupta <deepakgupta.bca22@poddarinstitute.org>
2025-01-27 22:08:44 +0530

    java script day1

---
+++ first/first.js
@@ -0,0 +1,65 @@
+console.log("Hello world");
+
+console.log("Good evening");
+
+
+name= "Deepak";
+
+console.log(name);
+name= "Ankit";
+
+console.log(name);
+
+a = 20;
+console.log(a);
+
+
+var  city = "jaipur";
+console.log(city);
+
+city = "delhi";
+console.log(city);
+
+// USING LET TO DECLARE A VARIABLE BECAUSE CAN NOT DECALRE A SAME NAME VARIABLE
+
+let age= 19;
+
+console.log(age);
+
+age = 20
+console.log(age);
+
+
+// let age = 20
+
+
+// Check the datatype using typeof variable_name;
+
+
+console.log(typeof age);
+
+let  n ="Ruhi";
+
+console.log(n);
+
+console.log(typeof n);
+
+//  CREATE A OBJECT     
+
+let student = 
+{
+    fname : "deepak",
+    age : 19,
+    course :"Python",
+    college: "Poddar"
+};
+
+console.log(student);
+
+console.log("First name is ",student["fname"]);
+
+
+student["age"] = 20;
+console.log(student)
+
+console.log(typeof student)
