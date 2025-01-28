dfeb72d0c8ae7edf0b842d660401a2ced8770727
Deepak gupta <deepakgupta.bca22@poddarinstitute.org>
2025-01-28 22:34:06 +0530

    Add files via upload

---
+++ second/script.js
@@ -0,0 +1,15 @@
+alert("hello!");
+
+number = prompt("enter the number ");
+
+console.log(number)
+
+if(number %2 ===0)
+{
+    console.log(number," Even number ");
+
+}
+else
+{
+    console.log(number ,"Odd Number");
+}
