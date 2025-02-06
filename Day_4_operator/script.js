1810d40adfb8f37be694f1a8d8828ff4820f46be
Deepak gupta <deepakgupta.bca22@poddarinstitute.org>
2025-02-06 21:32:10 +0530

    Add files via upload

---
+++ Day_4_operator/script.js
@@ -0,0 +1,91 @@
+// java script 
+
+// let a = 10
+
+
+// if(true)
+// {
+    
+// }
+// else{
+    
+// }
+
+let a = 10
+
+result = a >8 ? 'hello' : 'hii'
+
+console.log(result)
+
+
+
+
+let a = true
+let b = true
+
+if(a && b)
+{
+    console.log("Hello")
+}
+else{
+    console.log("hii")
+}
+
+
+// Or (||) operator
+
+let x = 5
+let y = 11
+
+if(x%2==0 || y%2==0)
+{
+    console.log("hello")
+}
+else{
+    console.log("Hii")
+}
+
+
+
+// Unary operator
+
+
+// convert string to integer using + method
+
+let a = "56"
+
+console.log(typeof a)
+
+a = +a
+console.log(typeof  a)
+
+
+
+// convert negative to positive 
+
+let b = -10
+
+console.log(-b)
+
+
+console.log(typeof b)
+
+// increment operator
+
+
+
+// let a = 12
+// let b  = ++a
+// let c = --a
+// let d = a++
+// let e = a--
+// console.log("b",b)
+// console.log("c",c)
+// console.log("d",d)
+// console.log("e",e)
+
+
+
+
+
+
