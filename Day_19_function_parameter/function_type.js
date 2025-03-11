fb12aaaf86ecc653843818a15ac918960ede1d67
Deepak gupta <deepakgupta.bca22@poddarinstitute.org>
2025-03-11 19:23:41 +0530

    function's type

---
+++ Day_19_function_parameter/function_type.js
@@ -0,0 +1,55 @@
+// // Named function 
+// function add(a,b)
+// {
+//     return a+b;
+
+// }
+// let a=  5;
+// let b=  6;
+// let ans = add(a,b);
+// console.log(ans);
+
+
+
+// //Expression function
+
+// const add = function(a,b){
+//     return a+b;
+// }
+// let a = 5;
+// let b  = 4;
+// let ans = add(a,b);
+// console.log(ans);
+
+
+
+// // Arrow function
+
+// const add = (a,b) =>{
+//     return a+b;
+
+// };
+// let a = 7;
+// let b = 3;
+// let ans  = add(a,b);
+// console.log(ans);
+
+
+
+// // lambda function :- concise and presie
+
+// const add = (a,b) =>a+b;
+// let a =5;
+// let b= 7;
+// let ans = add(a,b);
+
+// console.log(ans);
+
+
+
+// Q sum of two values
+
+const  add = (a,b) => a+b;
+add(4,5);
+console.log(add(4,5));
+
