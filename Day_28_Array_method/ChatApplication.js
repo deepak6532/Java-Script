d94ee326e5e84acacf904233733ecd8991fb083b
Deepak Gupta <deepakgupta.bca22@poddarinstitute.org>
2025-03-28 20:43:26 +0530

    chat application script.js

---
+++ Day_28_Array_method/ChatApplication.js
@@ -0,0 +1,35 @@
+let MessageBox  = []
+
+
+function addChat(...message)
+{
+    MessageBox.push(...message);
+}
+
+addChat("Hii","Who are you");
+
+
+
+// console.log(MessageBox)
+
+
+addChat("Hello ","I am Deepak ","And  you ?")
+
+
+// console.log(MessageBox)
+
+addChat("I am mahi ","your school friend ");
+
+addChat("Accha!")
+
+addChat("yes!")
+
+// addChat("Aaj kese yad aayi aapko hamari ")
+
+
+console.log(MessageBox);
+
+
+
+
+
